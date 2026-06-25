1877977bd53baef8670525734fcd9310e00be60c
Estevan Barbosa <estevan.saldanha26@gmail.com>
2026-06-25 12:17:17 -0300

    refatoraçãoo no Controller

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -6,7 +6,7 @@ class Controller {
     async listarRegistros(req, res) {
         try {
             const dados = await this.service.listarTodos()
-            return res.send(200).json(dados)
+            return res.status(200).json(dados)
         } catch (error) {
             return res.status(500).json({ erro: erro.message });
         }
@@ -31,21 +31,18 @@ class Controller {
         const criacaoDados = req.body 
         try {
             const dado = await this.service.criarRegistro(criacaoDados)
-            return res.send(200).json(dado)
+            return res.status(201).json(dado)
         } catch (error) {
             return res.status(500).json({ erro: erro.message });
         }
     }
    
     async atualizarUmRegistro(req, res) {
-        const id = req.params
+        const { id } = req.params
         const dadosAtualizados = req.body 
         try {
             const dado = await this.service.atualizarRegistro(id, dadosAtualizados)
-            if (!dado) {
-                return res.status(400).json({ mensagem: 'não foi atualizado' });
-            }
-            return res.send(200).json(dado)
+            return res.status(200).json(dado)
         } catch (error) {
             return res.status(500).json({ erro: erro.message });
         }
