12696f83754e32949eeec73b3266c2925fb6ff8e
Estevan <estevan.saldanha26@gmail.com>
2026-08-09 02:09:00 -0300

    feat: starting docker

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,7 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
-const result = dotenv.config();
 
 console.log('HOST:', process.env.MYSQLHOST);
 console.log('PORT:', process.env.MYSQLPORT);
-console.log(result);
 
 const sequelize = new Sequelize(
   process.env.MYSQLDATABASE,
